0c88f2493661dc520780c96ee59676fe7e0ab467
SaqibAwan143 <shehzadsaqib424@gmail.com>
2023-09-30 10:33:57 +0500

    chapter 7 different animations

---
+++ chap7_animations/android/app/src/main/kotlin/com/example/chap7_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chap7_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chap7_animations/lib/pages/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:chap7_animations/widgets/animated_container.dart';
+import 'package:chap7_animations/widgets/animated_cross_fade.dart';
+import 'package:chap7_animations/widgets/animated_opacity.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          title: Text('Home'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
+          flexibleSpace: SafeArea(
+            child: Icon(
+              Icons.photo_camera,
+              size: 75.0,
+              color: Colors.white70,
+            ),
+          ),
+          bottom: PreferredSize(
+            child: Container(
+              color: Colors.lightGreen.shade100,
+              height: 75.0,
+              width: double.infinity,
+              child: Center(
+                child: Text('Bottom'),
+              ),
+            ),
+            preferredSize: Size.fromHeight(75.0),
+          )),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                AnimatedContainerWidget(),
+                Divider(),
+                AnimatedCrossFadeWidget(),
+                Divider(),
+                AnimatedOpacityWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
