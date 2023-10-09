e18fecc321b94bcc8f63bddebf49bc5c2c314995
SaqibAwan143 <shehzadsaqib424@gmail.com>
2023-10-09 14:55:28 +0500

    List View in Chapter 9

---
+++ chap9_listview/lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:chap9_listview/widgets/header.dart';
+import 'package:chap9_listview/widgets/row_with_card.dart';
+import 'package:chap9_listview/widgets/row.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('top app bar'),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return HeaderWidget(
+                key: UniqueKey(),
+              );
+            } else if (index >= 1 && index <= 3) {
+              return RowWithCardWidget(key: UniqueKey(), index: index);
+            } else {
+              return RowWidget(key: UniqueKey(), index: index);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
